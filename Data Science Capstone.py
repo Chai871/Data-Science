5a6d34fb4a3174372b5d155740b2ac7f37ece94a
Chai871 <65104160+Chai871@users.noreply.github.com>
2020-05-10 10:16:23 -0700

    Add files via upload

---
+++ Data Science Capstone.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Data Science Capstone first project
+
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+       
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
